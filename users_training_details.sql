550630ea94c6f25c84afaea1de4b00dd39d6fbab
Mostafa.Ahmed <Mostafa.fci.cs@gmail.com>
2023-06-17 20:01:46 +0200

    Users training query

---
+++ users_training_details.sql
@@ -0,0 +1,6 @@
+﻿Select training_details.user_id, user_name, training_id, training_date,count(training_id)
+from users join training_details 
+on users.user_id = training_details.user_id
+Group by training_details.user_id,  user_name , training_id , training_date
+Having count(training_id)>1
+order by training_date desc;
